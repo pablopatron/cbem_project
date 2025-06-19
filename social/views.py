e9fef3d2af7bfa5615327bb9c5ab1840d2e0fabd
PhilTech-pixel <njoromanga49@gmail.com>
2025-06-19 10:09:28 +0300

    Implement API routes

--- social/views.py
+++ social/views.py
@@ -1,6 +1,8 @@
 # social/views.py
 from django.shortcuts import render
 
+#Implement restful API routes to access the posts, comments etc.
+
 def home(request):
     return render(request, 'social/home.html')
 
